7fde1c73a081ab71f7968f0a4a9e3099da7f6bf5
Cédric <cedricmoyaert@hotmail.com>
2022-09-19 11:04:46 +0200

    Added compatibility for the Google Chrome version of the extension

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
@@ -18,17 +19,12 @@ namespace BreezClickOnceHelper
 #if DEBUG
                 File.AppendAllLines("C:\\Temp\\breezclickoncedebug.txt", commandLineArgs);
 #endif
-                var installFlag = false;
                 var uninstallFlag = false;
                 var silentFlag = false;
                 var startedFromBrowser = false;
                 for (var index = 1; index < commandLineArgs.Length; ++index)
                 {
-                    if (commandLineArgs[index].Equals("/Install", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        installFlag = true;
-                    }
-                    else if (commandLineArgs[index].Equals("/Uninstall", StringComparison.InvariantCultureIgnoreCase))
+                    if (commandLineArgs[index].Equals("/Uninstall", StringComparison.InvariantCultureIgnoreCase))
                     {
                         uninstallFlag = true;
                     }
@@ -37,14 +33,38 @@ namespace BreezClickOnceHelper
                         silentFlag = true;
                     }
                 }
-                if (commandLineArgs.Length == 3 && commandLineArgs[1] == ManifestFileName && commandLineArgs[2] == ExtensionId)
+                if (commandLineArgs.Any(arg => arg.Contains(GetExtensionId(BrowserEnum.MozillaFirefox))) || commandLineArgs.Any(arg => arg.Contains(GetExtensionId(BrowserEnum.GoogleChrome))))
                 {
                     startedFromBrowser = true;
                 }
 
-                if (installFlag)
+                if (startedFromBrowser)
                 {
-                    Install(silentFlag);
+                    var str = OpenStandardStreamIn();
+
+                    //Check installation status
+                    if (str.StartsWith("{\"message\":\"", StringComparison.InvariantCultureIgnoreCase) && str.EndsWith("\"}", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var message = str.Substring("{\"message\":\"".Length, str.Length - "{\"message\":\"".Length - "\"}".Length);
+                        if (message == "CheckStatus")
+                        {
+                            OpenStandardStreamOut("{\"result\":\"OK\"}");
+                        }
+                    }
+                    //Check for ClickOnce URL
+                    else if (str.StartsWith("{\"url\":\"", StringComparison.InvariantCultureIgnoreCase) && str.EndsWith("\"}", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var launchOk = LaunchClickOnce(str.Substring("{\"url\":\"".Length, str.Length - "{\"url\":\"".Length - "\"}".Length));
+
+                        if (launchOk)
+                        {
+                            OpenStandardStreamOut("{\"result\":\"OK\"}");
+                        }
+                        else
+                        {
+                            OpenStandardStreamOut("{\"result\":\"FAIL\"}");
+                        }
+                    }
                 }
                 else if (uninstallFlag)
                 {
@@ -52,51 +72,10 @@ namespace BreezClickOnceHelper
                 }
                 else
                 {
-                    var str = OpenStandardStreamIn();
-                    if (string.IsNullOrWhiteSpace(str))
-                    {
-                        if (!IsInstalled)
-                        {
-                            Install(silentFlag);
-                        }
-                        else
-                        {
-                            Uninstall(silentFlag);
-                        }
-                    }
-                    else
-                    {
-                        if (startedFromBrowser)
-                        {
-                            //Check installation status
-                            if (str.StartsWith("{\"message\":\"", StringComparison.InvariantCultureIgnoreCase) && str.EndsWith("\"}", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                var message = str.Substring("{\"message\":\"".Length, str.Length - "{\"message\":\"".Length - "\"}".Length);
-                                if (message == "CheckStatus")
-                                {
-                                    OpenStandardStreamOut("{\"result\":\"OK\"}");
-                                }
-                            }
-                            //Check for ClickOnce URL
-                            else if (str.StartsWith("{\"url\":\"", StringComparison.InvariantCultureIgnoreCase) && str.EndsWith("\"}", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                var launchOk = LaunchClickOnce(str.Substring("{\"url\":\"".Length, str.Length - "{\"url\":\"".Length - "\"}".Length));
-
-                                if (launchOk)
-                                {
-                                    OpenStandardStreamOut("{\"result\":\"OK\"}");
-                                }
-                                else
-                                {
-                                    OpenStandardStreamOut("{\"result\":\"FAIL\"}");
-                                }
-                            }
-                                
-                        }
-                    }
+                    Install(silentFlag);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 #if DEBUG
                 File.AppendAllLines("C:\\Temp\\breezclickoncedebug.txt", new[] { ex.ToString() });
@@ -144,13 +123,13 @@ namespace BreezClickOnceHelper
         {
             try
             {
-                Process.Start("rundll32.exe", "dfshim.dll,ShOpenVerbApplication " + url).WaitForExit();
+                Process.Start("rundll32.exe", $"dfshim.dll,ShOpenVerbApplication {url}").WaitForExit();
                 return true;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 #if DEBUG                
-                File.AppendAllLines("C:\\Temp\\breezclickoncedebug.txt", new[] { ex.ToString() });
+                File.AppendAllLines(@"C:\Temp\breezclickoncedebug.txt", new[] { ex.ToString() });
 #endif
                 return false;
             }
@@ -160,17 +139,40 @@ namespace BreezClickOnceHelper
         {
             try
             {
+                //Determine executable path
                 var location = Assembly.GetExecutingAssembly().Location;
-                
-                var str2 = string.Format(string.Join("\n", NativeManifest));
-                Directory.CreateDirectory(InstallPath);
-                File.Copy(location, ExecutableFileName, true);
-                using (var streamWriter = new StreamWriter(ManifestFileName, false))
+
+                //Create target directory
+                if (!Directory.Exists(InstallPath))
                 {
-                    streamWriter.Write(str2);
+                    Directory.CreateDirectory(InstallPath);
                 }
-                Registry.SetValue($"HKEY_CURRENT_USER\\Software\\Mozilla\\NativeMessagingHosts\\{NativeMessageName}", null, ManifestFileName);
-                var keyName = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + ExecutableName;
+
+                //Install breezclickoncehelper.exe
+                File.Copy(location, ExecutableFileName, true);
+
+                //Install manifest file for Mozilla Firefox
+                var manifestFileNameFirefox = GetManifestFileName(BrowserEnum.MozillaFirefox);
+                using (var streamWriter = new StreamWriter(manifestFileNameFirefox, false))
+                {
+                    streamWriter.Write(GetNativeManifest(BrowserEnum.MozillaFirefox));
+                }
+
+                //Install manifest file for Google Chrome
+                var manifestFileNameGoogleChrome = GetManifestFileName(BrowserEnum.GoogleChrome);
+                using (var streamWriter = new StreamWriter(manifestFileNameGoogleChrome, false))
+                {
+                    streamWriter.Write(GetNativeManifest(BrowserEnum.GoogleChrome));
+                }
+
+                //Registry key for Mozilla Firefox
+                Registry.SetValue($@"HKEY_CURRENT_USER\Software\Mozilla\NativeMessagingHosts\{NativeMessageName}", null, manifestFileNameFirefox);
+
+                //Registry key for Google Chrome
+                Registry.SetValue($@"HKEY_CURRENT_USER\Software\Google\Chrome\NativeMessagingHosts\{NativeMessageName}", null, manifestFileNameGoogleChrome);
+
+                //Registry key for installed applications
+                var keyName = $@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Uninstall\{ExecutableName}";
                 Registry.SetValue(keyName, "Comments", ApplicationDescription);
                 Registry.SetValue(keyName, "DisplayName", ApplicationDescription);
                 Registry.SetValue(keyName, "DisplayIcon", ExecutableFileName);
@@ -179,8 +181,8 @@ namespace BreezClickOnceHelper
                 Registry.SetValue(keyName, "Publisher", "Breez");
                 Registry.SetValue(keyName, "NoModify", 1);
                 Registry.SetValue(keyName, "NoRepair", 1);
-                Registry.SetValue(keyName, "UninstallString", ExecutableFileName + " /Uninstall");
-                Registry.SetValue(keyName, "URLInfoAbout", "http://breezie.be");
+                Registry.SetValue(keyName, "UninstallString", $"{ExecutableFileName} /Uninstall");
+                Registry.SetValue(keyName, "URLInfoAbout", "https://breezie.be/dev/clickonce");
                 if (!silent)
                 {
                     MessageBox.Show("Breez ClickOnce Helper was installed successfully", "Information");
@@ -190,7 +192,7 @@ namespace BreezClickOnceHelper
             {
                 if (!silent)
                 {
-                    MessageBox.Show("There was an error installing Breez ClickOnce Helper:\n" + ex.Message, "Error");
+                    MessageBox.Show($"There was an error installing Breez ClickOnce Helper:\n{ex.Message}", "Error");
                 }
             }
         }
@@ -199,15 +201,52 @@ namespace BreezClickOnceHelper
         {
             try
             {
-                var subkey1 = $"Software\\Mozilla\\NativeMessagingHosts\\{NativeMessageName}";
-                Registry.CurrentUser.DeleteSubKeyTree(subkey1);
-                var subkey2 = "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\" + ExecutableName;
-                Registry.CurrentUser.DeleteSubKeyTree(subkey2);
-                File.Delete(ManifestFileName);
+                //Remove registry key for Mozilla Firefox
+                var subkeyFirefox = $@"Software\Mozilla\NativeMessagingHosts\{NativeMessageName}";
+                Registry.CurrentUser.DeleteSubKeyTree(subkeyFirefox);
+
+                //Remove registry key for Google Chrome
+                var subkeyChrome = $@"Software\Google\Chrome\NativeMessagingHosts\{NativeMessageName}";
+                Registry.CurrentUser.DeleteSubKeyTree(subkeyChrome);
+
+                //Remove registry key for installed applications
+                var subkeyUninstall = $@"Software\Microsoft\Windows\CurrentVersion\Uninstall\{ExecutableName}";
+                Registry.CurrentUser.DeleteSubKeyTree(subkeyUninstall);
+
+                //Remove manifest file for Mozilla Firefox
+                var manifestFileNameFirefox = GetManifestFileName(BrowserEnum.MozillaFirefox);
+                if (File.Exists(manifestFileNameFirefox))
+                {
+                    try
+                    {
+                        File.Delete(manifestFileNameFirefox);
+                    }
+                    catch
+                    {
+                        //Ignore
+                    }
+                }
+
+                //Remove manifest file for Google Chrome
+                var manifestFileNameChrome = GetManifestFileName(BrowserEnum.GoogleChrome);
+                if (File.Exists(manifestFileNameChrome))
+                {
+                    try
+                    {
+                        File.Delete(manifestFileNameChrome);
+                    }
+                    catch
+                    {
+                        //Ignore
+                    }
+                }
+
                 if (!silent)
                 {
                     MessageBox.Show("Breez ClickOnce Helper was uninstalled successfully", "Information");
                 }
+
+                //Remove breezclickoncehelper.exe
                 Process.Start(new ProcessStartInfo()
                 {
                     Arguments = $"/C choice /C Y /N /D Y /T 3 & rd /S /Q \"{InstallPath}\"",
@@ -220,18 +259,8 @@ namespace BreezClickOnceHelper
             {
                 if (!silent)
                 {
-                    MessageBox.Show("There was an error uninstalling Breez ClickOnce Helper:\n" + ex.Message, "Error");
+                    MessageBox.Show($"There was an error uninstalling Breez ClickOnce Helper:{ex.Message}", "Error");
                 }
-            }
-        }
-        
-        #region Consts and Properties
-        private static bool IsInstalled
-        {
-            get
-            {
-                var installed = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall\" + ExecutableName, false) != null;
-                return installed;
             }
         }
 
@@ -256,28 +285,81 @@ namespace BreezClickOnceHelper
             get { return Path.Combine(InstallPath, ExecutableName); }
         }
 
-        private static string ManifestFileName
+        private static string GetManifestFileName(BrowserEnum browser)
         {
-            get { return Path.Combine(InstallPath, ManifestName); }
+            return Path.Combine(InstallPath, GetManifestName(browser));
         }
-
-        private const string ManifestName = "breezclickonce_nm_manifest.json";
-
-        private const string ExtensionId = "breezclickonce@breezie.be";
 
         private const string NativeMessageName = "breez.clickonce.clickoncehelper";
 
         private const string ApplicationDescription = "Breez ClickOnce Helper";
 
-        private static readonly string[] NativeManifest = {
-            "{{",
-            $"  \"name\": \"{NativeMessageName}\",",
-            $"  \"description\": \"{ApplicationDescription}\",",
-            $"  \"path\": \"{ExecutableFileName.Replace("\\", "\\\\")}\",",
-            "  \"type\": \"stdio\",",
-            $"  \"allowed_extensions\": [\"{ExtensionId}\"]",
-            "}}"
-        };
-        #endregion
+        private static string GetManifestName(BrowserEnum browser){
+            const string ManifestNameFirefox = "breezclickonce_nm_manifest.json";
+            const string ManifestNameChrome = "breezclickonce_nm_manifest_chrome.json";
+            switch (browser)
+            {
+                case BrowserEnum.MozillaFirefox:
+                    return ManifestNameFirefox;
+                case BrowserEnum.GoogleChrome:
+                    return ManifestNameChrome;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static string GetExtensionId(BrowserEnum browser)
+        {
+            const string ExtensionIdFirefox = "breezclickonce@breezie.be";
+            const string ExtensionIdChrome = "cmkpnegkacdeagdmnbbhjbgdpndmlkgj"; //Store
+            //const string ExtensionIdChrome = "dehneackjjjjnhbfemfoehffbaionkcd"; //Debug
+
+            switch (browser)
+            {
+                case BrowserEnum.MozillaFirefox:
+                    return ExtensionIdFirefox;
+                case BrowserEnum.GoogleChrome:
+                    return ExtensionIdChrome;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Gets the json content for a native messaging file for the specified browser.
+        /// </summary>
+        /// <param name="browser">Mozilla Firefox or Google Chrome</param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private static string GetNativeManifest(BrowserEnum browser)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"{{");
+            sb.AppendLine($"  \"name\": \"{NativeMessageName}\",");
+            sb.AppendLine($"  \"description\": \"{ApplicationDescription}\",");
+            sb.AppendLine($"  \"path\": \"{ExecutableFileName.Replace("\\", "\\\\")}\",");
+            sb.AppendLine($"  \"type\": \"stdio\",");
+            switch (browser)
+            {
+                case BrowserEnum.MozillaFirefox:
+                    sb.AppendLine($"  \"allowed_extensions\": [\"{GetExtensionId(BrowserEnum.MozillaFirefox)}\"]");
+                    break;
+                case BrowserEnum.GoogleChrome:
+                    sb.AppendLine($"  \"allowed_origins\": [\"chrome-extension://{GetExtensionId(BrowserEnum.GoogleChrome)}/\"]");
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            sb.AppendLine($"}}");
+
+            return sb.ToString();
+        }
+    }
+
+    public enum BrowserEnum
+    {
+        MozillaFirefox = 1,
+        GoogleChrome = 2
     }
 }
